199f9ef08681c1f3b75a73a427583f92119365d1
Alex Shillingford <alex.shillingford@icloud.com>
2020-01-24 15:24:36 -0700

    Set up Navigation Between List and Detail, Pass Data into Child Views, Generate Previews Dynamically

--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -12,7 +12,7 @@ struct LandmarkList: View {
     var body: some View {
         NavigationView {
             List(landmarkData) { landmark in
-                NavigationLink(destination: LandmarkDetail()) {
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                     LandmarkRow(landmark: landmark)
                 }
             }
@@ -23,6 +23,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE", "iPhone 11 Pro Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
